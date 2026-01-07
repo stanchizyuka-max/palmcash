00d94add55684343d05b0076ecc343e3cf8b25bd
Doreen <munachizyuka@gmail.com>
2026-01-07 10:51:43 +0100

    Fix: Add PythonAnywhere domain to CSRF_TRUSTED_ORIGINS

--- palmcash/palmcash/settings.py
+++ palmcash/palmcash/settings.py
@@ -48,6 +48,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://stan13.pythonanywhere.com',
 ]
 
 # Palm Cash System Configuration
