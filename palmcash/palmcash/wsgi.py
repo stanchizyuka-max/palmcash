c293de6179664483e9b51d931f5f06d42c663206
Doreen <munachizyuka@gmail.com>
2026-01-07 12:37:40 +0100

    Add dependency fix guide and improve WSGI error handling

--- palmcash/palmcash/wsgi.py
+++ palmcash/palmcash/wsgi.py
@@ -10,15 +10,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "palmcash.settings")
 
-# Run migrations on startup
+# Get WSGI application first
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Run migrations on startup (after app is initialized)
 try:
+    from django.core.management import call_command
     call_command('migrate', verbosity=0, interactive=False)
 except Exception as e:
     print(f"Migration error: {e}", file=sys.stderr)
-
-application = get_wsgi_application()
+    # Don't fail the app startup if migrations fail
