36eb2e2bdfe4eb8efb88871336f353d6ec5ae9ba
Doreen <munachizyuka@gmail.com>
2026-01-07 12:50:50 +0100

    Remove migration runner from WSGI to fix redirect loop

--- palmcash/palmcash/wsgi.py
+++ palmcash/palmcash/wsgi.py
@@ -8,18 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "palmcash.settings")
 
-# Get WSGI application first
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Run migrations on startup (after app is initialized)
-try:
-    from django.core.management import call_command
-    call_command('migrate', verbosity=0, interactive=False)
-except Exception as e:
-    print(f"Migration error: {e}", file=sys.stderr)
-    # Don't fail the app startup if migrations fail
+
