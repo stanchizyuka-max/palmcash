483c6cdf44bdcf94d98aad18f713dfb9f90c4e85
Doreen <munachizyuka@gmail.com>
2026-02-17 13:22:59 +0100

    Remove CSRF_FAILURE_VIEW to fix import error

--- palmcash/palmcash/settings.py
+++ palmcash/palmcash/settings.py
@@ -72,9 +72,6 @@ CSRF_TRUSTED_ORIGINS = [
     'https://139.177.199.13',
 ]
 
-# CSRF Failure View
-CSRF_FAILURE_VIEW = 'common.views.csrf_failure'
-
 # Palm Cash System Configuration
 SYSTEM_LAUNCH_DATE = '2025-09-30'  # System development completion date
 
